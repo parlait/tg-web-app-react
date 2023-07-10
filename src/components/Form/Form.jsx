ae44bcec6fc446a78b16186117de6ca550d82e9d
parlait <fedorova.sv.a@gmail.com>
2023-07-10 13:28:05 +0300

    Changes to be committed: 	modified:   src/components/Form/Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -10,21 +10,37 @@ import './Form.css';
 
  
 const Form = () => {
+    const [country, setCountry] = useState('');
+    const [street, setStreet] = useState('');
+    const [subject, setSubject] = useState('physical');
     
+    const onChangeCountry = (e) => {
+        setCountry(e.target.value)
+    }
+    const onChangeStreet = (e) => {
+        setStreet(e.target.value)
+    }
+    const onChangeSubject = (e) => {
+        setSubject(e.target.value)
+    }
     return (
         <div className={"form"}>
             <h3>Введите ваши данные</h3>
             <input
             className={'input'}
             type="text"
-            placeholder={'Страна'}
+            placeholder={'Страна'} 
+            value={country}
+            onChange={onChangeCountry}
             />
             <input
             className={'input'}
             type="text"
             placeholder={'Улица'}
+            value={street}
+            onChange={onChangeStreet}
             />
-            <select className={'select'}>
+            <select value={subject} onChange={onChangeSubject} className={'select'}>
                 <option value={'physical'}>Физ. лицо</option>
                 <option value={'legal'}>Юр. лицо</option>
             </select>
