ad1381bd1a4fdf18017d10aab6a7a9c2a24d2d2a
parlait <fedorova.sv.a@gmail.com>
2023-07-10 18:13:18 +0300

    modified:   src/components/Form/Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -46,8 +46,7 @@ const Form = () => {
     const onChangeSubject = (e) => {
         setSubject(e.target.value)
     }
-    /*
-    */
+
     return (
         <div className={"form"}>
             <h3>Введите ваши данные</h3>
@@ -55,21 +54,21 @@ const Form = () => {
             className={'input'} 
             type="text" 
             placeholder={'Страна'}
-            /* 
+         
             value={country} 
             onChange={onChangeCountry} 
-            */
+          
             />
             <input 
             className={'input'} 
             type="text" 
             placeholder={'Улица'} 
-            /*
+        
             value={street} 
             onChange={onChangeStreet} 
-            */
+       
             />
-            <select className={'select'}>
+            <select value={subject} onChange={onChangeSubject} className={'select'}>
                 <option value={'physical'}>Физ. лицо</option>
                 <option value={'legal'}>Юр. лицо</option>
             </select>
