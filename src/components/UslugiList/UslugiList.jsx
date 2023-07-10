ceeab35f8acb77bca2fc0475e1e101a10cccac33
parlait <fedorova.sv.a@gmail.com>
2023-07-10 17:40:17 +0300

    Changes to be committed: modified:   src/components/UslugiList/UslugiList.jsx

--- src/components/UslugiList/UslugiList.jsx
+++ src/components/UslugiList/UslugiList.jsx
@@ -1,75 +1,22 @@
 import React from 'react';
 import './UslugiList.css';
-import {useTelegram} from '../hooks/useTelegram';
+//import { useTelegram } from '../hooks/useTelegram';
 // className={'header'}>
 //     <Button onClick={onClose}>Закрыть</Button>
 //     <span className={'username'}>
 //         {user?.username}
 //     </span>
-//const {user, onClose} = useTelegram();
+ //const {user, onClose} = useTelegram();
+const UslugiList = () => {
+    
 
- 
-const Form = () => {
-    
-    const [country, setCountry] = useState('');
-    const [street, setStreet] = useState('');
-    const [subject, setSubject] = useState('physical');
-    
-    const {tg} = useTelegram();
-    useEffect( () => {
-        tg.MainButton.setParams({
-            text: 'Отправить данные'
-        })
-
-    },[])
-/*
-    useEffect( () => {
-            if(!street || !country) {
-                tg.mainButton.hide();
-            } else {
-                tg.mainButton.show();
-            }
-    },[country, street])
-    
-    const onChangeCountry = (e) => {
-        setCountry(e.target.value)
-    }
-    const onChangeStreet = (e) => {
-        setStreet(e.target.value)
-    }
-    const onChangeSubject = (e) => {
-        setSubject(e.target.value)
-    }
-    */
     return (
-        <div className={"form"}>
-            <h3>Выберите услуги, на которые хотите записаться</h3>
-            <input
-            className={'input'}
-            type="text"
-            placeholder={'Страна'}
-            /* 
-            value={country}
-            onChange={onChangeCountry}
-            */
-            />
-            <input
-            className={'input'}
-            type="text"
-            placeholder={'Улица'}
-            /*
-            value={street}
-            onChange={onChangeStreet}
-            */
-            />
-            <select className={'select'}>
-                <option value={'physical'}>Физ. лицо</option>
-                <option value={'legal'}>Юр. лицо</option>
-            </select>
+        <div>
+            ProductList
         </div>
     );
 
-//select value={subject} onChange={onChangeSubject} className
+
 };
 
 export default UslugiList;
