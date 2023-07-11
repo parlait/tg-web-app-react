d5e472d3c90b5b57e6733c5e05e1418fceb356ac
parlait <fedorova.sv.a@gmail.com>
2023-07-11 17:29:33 +0300

    Changes to be committed: 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -48,8 +48,8 @@ function App() {
         <Route path={'formsotrudnik'} element={<FormSotrudnik />}/>
         <Route path={'formusluga'} element={<FormUsluga />}/>
         <Route path={'formposeschenie'} element={<FormPoseschenie />}/>
-        <Route path={'uslugilist'} element={<UslugiList />}/>
-        <Route path={'uslugilistvnes'} element={<UslugiListVnesSotr />}/>
+        <Route path={'uslugilist/*'} element={<UslugiList />}/>
+        <Route path={'uslugilistvnessotr/*'} element={<UslugiListVnesSotr />}/>
         <Route path={'formdolzhnosti'} element={<FormDolzhnosti />}/>
       </Routes>
     
