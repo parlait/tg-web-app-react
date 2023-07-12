e98a3e5ae13c80b089fe6ae0571840f87d2c8844
parlait <fedorova.sv.a@gmail.com>
2023-07-12 14:43:09 +0300

    Changes to be committed: 	modified:   src/components/ProductList/ProductList.jsx

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,9 +1,15 @@
+/*
 import React, {useState} from 'react';
 import './ProductList.css';
 import ProductItem from "../ProductItem/ProductItem";
 import {useTelegram} from "../../hooks/useTelegram";
 import {useCallback, useEffect} from "react";
-
+*/
+import React from 'react';
+import './ProductList.css';
+import ProductItem from '../ProductItem/ProductItem.jsx';
+import { useTelegram } from '../hooks/useTelegram';
+import {useCallback, useEffect} from 'react';
 const products = [
     {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
     {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
