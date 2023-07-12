547b58bac8b775cc175d81bb5353c27c353d4234
parlait <fedorova.sv.a@gmail.com>
2023-07-12 15:32:15 +0300

    modified:   src/components/ProductList/ProductList.jsx

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -4,6 +4,8 @@ import {useEffect} from 'react';
 import {useCallback} from 'react';
 import './ProductList.css';
 import {useTelegram} from '../hooks/useTelegram';
+import ProductItem from '../ProductItem/ProductItem.jsx';
+
 
 // className={'header'}>
 //     <Button onClick={onClose}>Закрыть</Button>
@@ -107,11 +109,7 @@ export default ProductList;
 
 /*
 
-import React from 'react';
-import './ProductList.css';
-import ProductItem from '../ProductItem/ProductItem.jsx';
-import {useTelegram} from '../hooks/useTelegram';
-import {useCallback, useEffect} from 'react';
+
 const products = [
     {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
     {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
