1eda1f2cfd60e7de3b5c1618b9879949742e7878
parlait <fedorova.sv.a@gmail.com>
2023-07-12 15:33:45 +0300

    modified:   src/components/ProductList/ProductList.jsx

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -6,13 +6,16 @@ import './ProductList.css';
 import {useTelegram} from '../hooks/useTelegram';
 import ProductItem from '../ProductItem/ProductItem.jsx';
 
-
-// className={'header'}>
-//     <Button onClick={onClose}>Закрыть</Button>
-//     <span className={'username'}>
-//         {user?.username}
-//     </span>
-//const {user, onClose} = useTelegram();
+const products = [
+    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
+    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
+    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
+    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
+    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
+]
 
  
 const ProductList = () => {
@@ -110,16 +113,7 @@ export default ProductList;
 /*
 
 
-const products = [
-    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
-    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
-    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
-]
+
 
 const getTotalPrice = (items = []) => {
     return items.reduce((acc, item) => {
