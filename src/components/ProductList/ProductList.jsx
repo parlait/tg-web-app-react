77ed95326a75256eb1e74321a2517466e1ddc25b
parlait <fedorova.sv.a@gmail.com>
2023-07-12 15:29:56 +0300

    modified:   src/components/ProductList/ProductList.jsx

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,10 +1,112 @@
-/*
-import React, {useState} from 'react';
+import React from 'react';
+import {useState} from 'react';
+import {useEffect} from 'react';
+import {useCallback} from 'react';
 import './ProductList.css';
-import ProductItem from "../ProductItem/ProductItem";
-import {useTelegram} from "../../hooks/useTelegram";
-import {useCallback, useEffect} from "react";
-*/
+import {useTelegram} from '../hooks/useTelegram';
+
+// className={'header'}>
+//     <Button onClick={onClose}>Закрыть</Button>
+//     <span className={'username'}>
+//         {user?.username}
+//     </span>
+//const {user, onClose} = useTelegram();
+
+ 
+const ProductList = () => {
+    
+    const [country, setCountry] = useState('');
+    const [street, setStreet] = useState('');
+    const [subject, setSubject] = useState('physical');
+    
+    const {tg} = useTelegram();
+    
+
+    const onSendData = useCallback( () =>{ 
+        const data = {
+            country,
+            street,
+            subject
+        }
+        tg.sendData(JSON.stringify(data));
+    },[country,street,subject])
+    useEffect( () => {
+        tg.onEvent('mainButtonClicked',onSendData)
+        return () =>{ 
+            tg.offEvent('mainButtonClicked',onSendData)
+        }
+    },[onSendData])
+    
+
+
+
+    useEffect( () => {
+        tg.MainButton.setParams({
+            text: 'Отправить данные'
+        })
+
+    },[])
+   
+
+
+    useEffect( () => {
+            if(!street || !country) {
+                tg.MainButton.hide();
+            } else {
+                tg.MainButton.show();
+            }
+    },[country, street])
+     
+    const onChangeCountry = (e) => {
+        setCountry(e.target.value)
+    }
+    const onChangeStreet = (e) => {
+        setStreet(e.target.value)
+    }
+    const onChangeSubject = (e) => {
+        setSubject(e.target.value)
+    }
+
+    return (
+        <div className={"form"}>
+            <h3>Введите ваши данные</h3>
+            <input 
+            className={'input'} 
+            type="text" 
+            placeholder={'Страна'}
+         
+            value={country} 
+            onChange={onChangeCountry} 
+          
+            />
+            <input 
+            className={'input'} 
+            type="text" 
+            placeholder={'Улица'} 
+        
+            value={street} 
+            onChange={onChangeStreet} 
+       
+            />
+            <select value={subject} onChange={onChangeSubject} className={'select'}>
+                <option value={'physical'}>Физ. лицо</option>
+                <option value={'legal'}>Юр. лицо</option>
+            </select>
+        </div>
+    );
+//select value={subject} onChange={onChangeSubject} className=
+
+};
+
+export default ProductList;
+
+
+
+
+
+
+/*
+
 import React from 'react';
 import './ProductList.css';
 import ProductItem from '../ProductItem/ProductItem.jsx';
@@ -56,7 +158,7 @@ const ProductList = () => {
             tg.offEvent('mainButtonClicked', onSendData)
         }
     }, [onSendData])
-*/
+
     const onAdd = (product) => {
         const alreadyAdded = addedItems.find(item => item.id === product.id);
         let newItems = [];
@@ -93,6 +195,7 @@ const ProductList = () => {
 };
 
 export default ProductList;
+*/
 /*
 import React from 'react';
 import './ProductList.css';
