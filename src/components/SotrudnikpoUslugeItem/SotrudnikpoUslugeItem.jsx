b6c391cf510f847e9d0c0ee1efc8f1642f448baf
parlait <fedorova.sv.a@gmail.com>
2023-07-13 01:24:46 +0300

    Changes to be committed: 	deleted:    src/components/DopUslugiKUsluge/DopUslugiKUsluge.css 	deleted:    src/components/DopUslugiKUsluge/DopUslugiKUsluge.jsx 	deleted:    src/components/SotrudnikpoUslugeItem/SotrudnikpoUslugeItem.css 	deleted:    src/components/SotrudnikpoUslugeItem/SotrudnikpoUslugeItem.jsx 	modified:   src/components/UslugaItem/UslugaItem.jsx

--- src/components/SotrudnikpoUslugeItem/SotrudnikpoUslugeItem.jsx
+++
@@ -1,4 +0,0 @@
-<select value={subject} onChange={onChangeSubject} className={'select'}>
-<option value={'physical'}>Физ. лицо</option>
-<option value={'legal'}>Юр. лицо</option>
-</select>
--- src/components/UslugaItem/UslugaItem.jsx
+++ src/components/UslugaItem/UslugaItem.jsx
@@ -8,32 +8,54 @@ const UslugaItem = ({product, className, onAdd}) => {
         onAdd(product);
     }
 // <div className={'img'}/>
-
+if(vyborsotr==true) {"vybrat"}
+if(vybordop==true) {"vybrat"}
+if(pokazyvatcenu==true){"vybrat"}
+nuzhnoOpisanieUslugi=true
+nuzhnaCenaUslugi=true
     return (
         <div className={'product ' + className}>
-           
             <div className={'title'}>{product.title}</div>
-            <div className={'description'}>{product.description}</div>
-            <div className={'price'}>
-                <span>Стоимость: <b>{product.price}</b></span>
+            {nuzhnoOpisanieUslugi ? <div className={'description'}>{product.description}</div> : null}
+            {nuzhnaCenaUslugi ? <div className={'price'}><span>Стоимость: <b>{product.price}</b></span></div> : null}
+            <div className={'listdopopcii'}>
+            <select multiple name="myselectdopopcii" id={product.id + dop}>
+            {sotrudnkiuslugi.map(item => (
+            <option value={dopid}>
+                {imyadop}
+            </option>
+            ))}
+            </select>
             </div>
             <div className={'listsotrudnikovuslugi'}>
+            <select multiple name="myselectsotr" id={product.id + sotr}>
             {sotrudnkiuslugi.map(item => (
-            <SotrudnikpoUslugeItem
-                dolzhnost={item}
-                imya={onAdd}
-                className={'sotr'}
-            />
+            <option value={sotrid}>
+                {imyasotr}
+            </option>
             ))}
+            </select>
             </div>
             <Button className={'add-btn'} onClick={onAddHandler}>
-                Добавить в корзину
+                Выбрать
             </Button>
         </div>
     );
 };
-
 export default UslugaItem;
+//<select multiple name="myselect" id="myselect">
+//  <option value="1">option1</option>
+//  <option value="2">option2</option>
+//  <option value="3">option3</option>
+//  <option value="4">option4</option>
+//</select>
+/*
+//<input list="%идентификатор%">
+//<datalist id="%идентификатор%">
+//<option value="Город1">
+//<option value="Город2">
+//</datalist>
+*/
 /*
 import React from 'react';
 import Button from "../Button/Button";
