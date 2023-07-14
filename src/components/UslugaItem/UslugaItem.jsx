115e3f34cd1ce165d2ee9691bf79dc86e7b0202c
parlait <fedorova.sv.a@gmail.com>
2023-07-14 12:13:32 +0300

    modified:   src/components/UslugaItem/UslugaItem.jsx

--- src/components/UslugaItem/UslugaItem.jsx
+++ src/components/UslugaItem/UslugaItem.jsx
@@ -10,8 +10,8 @@ const UslugaItem = ({product, className, onAdd}) => {
 // <div className={'img'}/>
 
     return (
-        <p>ну хоть это покажет</p>
-       // <p>{item.n}</p>
+       // <p>ну хоть это покажет</p>
+        <p>{product.n}</p>
         /*
         <div className={'product ' + className}>
             <div className={'title'}>{product.n}</div>
